12e0d89abf37eaef502300954230226ee84b81db
Ignacio Cortez <ig.cortez@duocuc.cl>
2025-09-06 21:44:16 -0400

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'core/index.html')
+# Se define el index para que sea la página principal, este será nuestra página de inicio de la pagina web aquí, en views.py
+#El segundo paso será crear la url en url.py tanto de core como de Eltotem, y se crean las rutas
+#Tercer paso es agregar 'core' en settings.py de Eltotem en la parte de INSTALLED_APPS
