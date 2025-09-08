9e1c9435f39560b3f33b6a8a19f62f39f9edfeb8
Ignacio Cortez <ig.cortez@duocuc.cl>
2025-09-07 21:02:06 -0300

    Update urls.py
    algunos importes de login y auth, también validaciones

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('registrarse/', views.registrarse_view, name='registrarse'),
+    path('login/', views.login_view, name='login'),
 
 ]
