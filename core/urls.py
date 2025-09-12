c9a143377f1d580e93d49af0e2bebd19f8b85bb7
Ignacio Cortez <ig.cortez@duocuc.cl>
2025-09-12 18:54:40 -0300

    Update urls.py
    SE HABILITA PARA PODER HACER SCRAPPING

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('registrarse/', views.registrarse_view, name='registrarse'),
     path('login/', views.login_view, name='login'),
+    path('noticias/', views.NoticiasFiltradasView.as_view(), name='noticias'),
 
 ]
