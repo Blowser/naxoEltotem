04b531c109b546ba5ccdbf0da469b9fea6d25842
Ignacio Cortez <ig.cortez@duocuc.cl>
2025-09-12 21:12:20 -0300

    Update settings.py
    se conecta a bd  de MySQL

--- ElTotem/settings.py
+++ ElTotem/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'ElTotem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eltotem_db',
+        'USER': 'naxo_eltotem',
+        'PASSWORD': 'RitualClave_2025!',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
+
 
 
 # Password validation
