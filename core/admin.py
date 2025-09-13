df9bb6cea43db7a412c1a1d5ce1ea10817ce7ce0
Ignacio Cortez <ig.cortez@duocuc.cl>
2025-09-12 22:47:38 -0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import NoticiaTCG
 
-# Register your models here.
+@admin.register(NoticiaTCG)
+class NoticiaTCGAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'juego', 'tipo_evento', 'fecha')
+    search_fields = ('titulo', 'resumen')
+    list_filter = ('juego', 'tipo_evento', 'fecha')
