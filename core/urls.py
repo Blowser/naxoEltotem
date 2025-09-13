9cd58b69497d6820faa3a73af23f7b5c89f28075
Ignacio Cortez <ig.cortez@duocuc.cl>
2025-09-13 13:57:05 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('registrarse/', views.registrarse_view, name='registrarse'),
     path('login/', views.login_view, name='login'),
     path('noticias/', views.NoticiasFiltradasView.as_view(), name='noticias'),
+    path('scrap-yugioh/', views.scrap_yugioh, name='scrap_yugioh'),
 
 ]
